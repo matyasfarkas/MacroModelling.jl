b02a89bdcb2805a3da5d399ee6b920ba67e81387
fmatyas <matyi.farkas@gmail.com>
2023-07-24 22:58:37 +0200

    FF ZLB without anticipation in development

--- test/FF_ZLB_MCMC.jl
+++ test/FF_ZLB_MCMC.jl
@@ -208,7 +208,12 @@ for t = 1:periods
                  consthorizon = +1
             end
         end
+        if consthorizon > size(fgshlist, 1)
+            println("ZLB spell too long...")
+        end
+
         for hmax = size(fgshlist, 1)+1:-1:1
+
             if consthorizon == hmax
                 if (size(fgshlist, 1)+1+t > only(periods))
                     ϵ_wzlb[:, t:only(periods)] = ℱ.value.(ϵ[:, t:only(periods)])
@@ -263,7 +268,7 @@ StatsPlots.plot(simulated_data', label = ["INFL" "INT" "YGR"])
 
 #MacroModelling.plot_irf(AS07,shocks = shockstrue, periods = 0)
 #StatsPlots.plot(shocks')
-Ω = 10^(-4)# eps()
+Ω = 10^(-3)# eps()
 n_samples = 1000
 
 #=
@@ -477,6 +482,10 @@ Turing.@model function loglikelihood_scaling_function_ff(m, data, observables, �
                      consthorizon = +1
                 end
             end
+            if consthorizon > size(fgshlist, 1)
+                return Turing.@addlogprob! Inf
+                #println("ZLB spell too long...")
+            end
             for hmax = size(fgshlist, 1)+1:-1:1
                 if consthorizon == hmax
                     if (size(fgshlist, 1)+1+t > only(size(data,2)))
@@ -542,7 +551,7 @@ observables_index = sort(indexin(observables, m.timings.var))
 
 loglikelihood_scaling_ff = loglikelihood_scaling_function_ff(AS07, data, observables, Ω, zlbvar, zlblevel,fgshlist) # m, data, observables, Ω , zlbvar, zlblevel,fgshlist  # Filter free
 
-n_samples = 500
+n_samples = 100
 samps_ff = Turing.sample(loglikelihood_scaling_ff, Turing.NUTS(), n_samples, progress = true)#, init_params = sol
 
 
